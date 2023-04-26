ccac8782c926c10f6d33f20a5af7e748eb82910b
johnaroj@hotmail.com <johnaroj@hotmail.com>
2023-04-26 09:03:59 +0200

    update bugfixes

--- src/app/api/auth/[...nextauth]/route.ts
+++
@@ -1,5 +0,0 @@
-import NextAuth, { NextAuthOptions } from "next-auth";
-import { authOptions } from "@/lib/auth";
-
-export const handler: NextAuthOptions = NextAuth(authOptions);
-export { handler as GET, handler as POST };
